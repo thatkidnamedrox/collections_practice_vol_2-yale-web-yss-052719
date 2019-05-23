148753989f4865bbebd163b03c57b9973c15e421
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-23 21:22:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,6 +39,7 @@ def merge_data(keys, data)
     data.each do |h|
       h.find do |h_element|
         k_name = k_element.values.first
+        h_name = h_element.first
         puts h_element.inspect
         puts " "
 
