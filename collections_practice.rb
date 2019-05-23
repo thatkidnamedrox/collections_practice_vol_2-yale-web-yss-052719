032f1adb8d2ee63333a5f3a15fd9fc8c3806e33f
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-23 21:26:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,7 +44,7 @@ def merge_data(keys, data)
         k_name == h_name
       end
 
-      puts [d.inspect, k_element.inspect]
+      puts [d.values.inspect, k_element.inspect]
     end
   end
   result
