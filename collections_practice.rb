fd9849ce684f8bf8e2e5c238e4c56a5ae4e6653e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-23 21:19:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,7 @@ def merge_data(keys, data)
       d.find do |e|
         k = element.values.first
         e.keys
-        puts [e, k, d].inspect
+        puts [e, k].inspect
         k == d
       end
     end
